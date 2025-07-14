8ad2ed0abd53fdb6ae32abe83b8ead4b7e99ccc1
AliSuleman27 <muzamil.ali1099@gmail.com>
2025-07-15 03:36:29 +0500

    added

--- assets/login/auth.js
+++ assets/login/auth.js
@@ -16,43 +16,85 @@
       const card = document.getElementById('authCard');
       card.classList.toggle('flipped');
     }
-    PASSWORD = null;
-    EMAIL = null;
-    FULL_NAME = null;
+ 
 
-document.getElementById("registerBTN").onsubmit = async()=>{
+    
+    let PASSWORD = "";
+    let EMAIL = "";
+    let FULL_NAME = "";
+    
+    function clearVariables(){
+      PASSWORD = "";
+      EMAIL = "";
+      FULL_NAME = "";
+    }
+    
+    
 
-  PASSWORD = document.getElementById("registerPASS").value;
-  EMAIL = document.getElementById("registerEMAIL").value;
-  FULL_NAME = document.getElementById("registerFNAME").value;
+// REGISTER
+  let REG_PASSWORD = document.getElementById("registerPASS");
+  let REG_PASSWORD_CONFIRM = document.getElementById("registerPASSCONFIRM");
+  let REG_EMAIL = document.getElementById("registerEMAIL");
+  let REG_FULL_NAME = document.getElementById("registerFNAME");
 
+function regformClear(){
+    REG_PASSWORD.value = "";
+    REG_PASSWORD_CONFIRM.value = "";
+    REG_EMAIL.value = "";
+    REG_FULL_NAME.value = "";
+    }
 
-if(!PASSWORD || !EMAIL || !FULL_NAME){
-  console.log("PLEASE FILL ALL FIELDS");
-  // return;
+function isValidEmail(email) {
+  const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return pattern.test(email);
 }
 
 
- try {
+document.getElementById("signupForm").onsubmit = async(e)=>{
+  e.preventDefault();
+  clearVariables();
+if(REG_PASSWORD.value !== REG_PASSWORD_CONFIRM.value){
+  console.log("PASSWORD DONT MATCH")
+  return;
+}
+if(!isValidEmail(REG_EMAIL)){
+  console.log("PLEASE ENTER VALID EMAIL"); 
+}
+if(REG_PASSWORD.value < 6){
+  console.log("TOO SMALL, MINIMUM PASSWORD LENGTH IS 8");  
+
+}
+
+  PASSWORD = REG_PASSWORD.value;  
+  EMAIL = REG_EMAIL.value;
+  FULL_NAME = REG_FULL_NAME.value;
+
+
+
+try {
+
           const response = await fetch('http://127.0.0.1:8000/submit', {
             method: "POST",
             headers: {
-              'Content-Type': 'application/json',
+                "Cache-Control": "no-cache",
+                "Pragma": "no-cache",
+                'Content-Type': 'application/json',
             },
-            body: {'email': `${EMAIL}`}
+            body :JSON.stringify({ email: EMAIL })
           });
 
           const result = await response.json();
 
           if(result.status === "success"){
               console.log(result.message);
+
           }else{
             console.log(result.message);
           }
+          
   }
 catch{
 console.log("NETWORK ISSUE");
-
 }
 
 
